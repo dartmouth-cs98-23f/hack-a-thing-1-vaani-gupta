afc76e3512cfbf2835d9d11f9cb5ae3f30bbeef3
Vaani Gupta <Vaani.Gupta.24@dartmouth.edu>
2023-09-15 09:50:09 -0500

    Creating backend with simulated JSON server

--- first_tutorial/src/App.js
+++ first_tutorial/src/App.js
@@ -4,6 +4,7 @@ import Footer from './Footer';
 import { useState, useEffect } from 'react';
 import AddItem from './AddItem';
 import SearchItem from './SearchItem';
+import apiRequest from './apiRequest';
 
 function App() {
   const API_URL = 'http://localhost:3500/items';
@@ -43,21 +44,50 @@ function App() {
 
   }, [])
 
-  const addItem = (item) => {
+  const addItem = async (item) => {
     const id = items.length ? items[items.length - 1].id + 1 : 1;
     const inputNewItem = {id, checked: false, item};
     const listItems = [...items, inputNewItem];
     setItems(listItems);
+
+    const postOptions = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(inputNewItem)
+    };
+
+    const result = await apiRequest(API_URL, postOptions);
+    if (result) setFetchError(result);
   }
 
-  const handleCheckBox = (id) => {
+  const handleCheckBox = async (id) => {
     const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked} : item);
     setItems(listItems);
+
+    const myItem = listItems.filter((item) => item.id === id);
+    const updateOptions = {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({checked: myItem[0].checked})
+    };
+
+    const reqURL = `${API_URL}/${id}`;
+    const result = await apiRequest(reqURL, updateOptions);
+    if (result) setFetchError(result);
   }
 
-  const handleDelete = (id) => {
+  const handleDelete = async (id) => {
     const listItems = items.filter((item) => item.id !== id); 
     setItems(listItems);
+
+    const deleteOptions = { method: 'DELETE'};
+    const reqURL = `${API_URL}/${id}`;
+    const result = await apiRequest(reqURL, deleteOptions);
+    if (result) setFetchError(result);
   }
 
   const handleSubmit = (e) => {
